93f833b69d33cc20d44779bc6da0a61e09db91b4
jsiuda1 <jakkub68@gmail.com>
2022-08-29 22:27:37 +0200

    init

---
+++ c_types_in_struct.py
@@ -0,0 +1,4 @@
+class c_types(object):
+    def __init__(self) -> None:
+        self.name
+        self.bytes_size
---
+++ struct_decoder.py
@@ -0,0 +1,3 @@
+class StructDecoder(object):
+    def __init__(self):
+        pass
